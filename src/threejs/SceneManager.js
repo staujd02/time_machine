86e508e1bc2c2bab861ac68db4a1d4cb4808c7ef
Joel <staujd02@students.ipfw.edu>
2018-06-11 21:55:56 -0400

    shamed: fixed what I broke

--- src/threejs/SceneManager.js
+++ src/threejs/SceneManager.js
@@ -1,14 +1,20 @@
 /* Imported From: https://itnext.io/how-to-use-plain-three-js-in-your-react-apps-417a79d926e0 */
 import * as THREE from 'three';
 import DataPoint from './DataPoint';
+import * as dat from 'dat.gui';
 
 export default canvas => {
 
+    //const clock = new THREE.Clock();
     const origin = new THREE.Vector3(0,0,0);
 
     const screenDimensions = {
         width: canvas.width,
         height: canvas.height
+    }
+    
+    const controls = {
+        size: 40
     }
 
     var dataPointGeometry = new THREE.CircleGeometry( 40, 32 );
@@ -19,6 +25,17 @@ export default canvas => {
     const scene = buildScene();
     const renderer = buildRender(screenDimensions);
     const camera = buildCamera(screenDimensions);
+    const gui = buildGUI();
+    
+
+    function buildGUI(){
+        var gui = new dat.GUI();
+        var sizeController = gui.add(controls, 'size').min(10).max(100).step(1);
+        sizeController.onChange(function(newValue){
+            dataPointScale = newValue/40.0; //Ratio of original size (40)
+            update();
+        });
+    }
    
     function buildScene() {
         const scene = new THREE.Scene();
@@ -68,6 +85,18 @@ export default canvas => {
         }
         
         renderer.render(scene, camera);
+    }
+
+    function onWindowResize() {
+        /*const { width, height } = canvas;
+        
+        screenDimensions.width = width;
+        screenDimensions.height = height;
+
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+        
+        renderer.setSize(width, height);*/
     }
 
     return {
