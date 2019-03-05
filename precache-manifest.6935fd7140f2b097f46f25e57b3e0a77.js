86e1ca873c1b99f2329e8ce85f3124e0212db471
Joel <staujd02@students.ipfw.edu>
2019-03-05 14:17:15 -0500

    Update: flux, styling, and highlighting

---
+++ precache-manifest.6935fd7140f2b097f46f25e57b3e0a77.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f721466883998665b87923b92dea655b",
+    "url": "/time_machine/static/media/glyphicons-halflings-regular.f7214668.svg"
+  },
+  {
+    "revision": "54f21de5131a764c6b7c",
+    "url": "/time_machine/static/css/main.c144516d.chunk.css"
+  },
+  {
+    "revision": "15b3aaff769f0f9e4659",
+    "url": "/time_machine/static/js/runtime~main.15b3aaff.js"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/time_machine/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "9cf320176d75d994535c",
+    "url": "/time_machine/static/js/2.9cf32017.chunk.js"
+  },
+  {
+    "revision": "54f21de5131a764c6b7c",
+    "url": "/time_machine/static/js/main.54f21de5.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/time_machine/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/time_machine/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/time_machine/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "9cf320176d75d994535c",
+    "url": "/time_machine/static/css/2.82552781.chunk.css"
+  },
+  {
+    "revision": "2bcee59cf1498d150bbd463f0680946b",
+    "url": "/time_machine/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/time_machine/precache-manifest.2cb2ed319622cc73dad6c0bdaf76a593.js"
+  "/time_machine/precache-manifest.6935fd7140f2b097f46f25e57b3e0a77.js"
 );
 
 workbox.clientsClaim();
