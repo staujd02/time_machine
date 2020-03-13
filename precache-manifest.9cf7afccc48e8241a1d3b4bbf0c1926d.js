ac4da93c631ec871e104f25238957fdaa5f34ac4
Joel <staujd02@students.ipfw.edu>
2020-03-12 22:06:05 -0400

    Deploy: Clear Flux Feature, Flux Col. Row Fix, and Black Index Labels

---
+++ precache-manifest.9cf7afccc48e8241a1d3b4bbf0c1926d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2363cef2d0ee07a092877131dda7901",
+    "url": "/time_machine/index.html"
+  },
+  {
+    "revision": "b31767ff74c7f5e36f2b",
+    "url": "/time_machine/static/css/2.a8cba42d.chunk.css"
+  },
+  {
+    "revision": "9f5d2d66e5d1b3789210",
+    "url": "/time_machine/static/css/main.2dbbfdcd.chunk.css"
+  },
+  {
+    "revision": "b31767ff74c7f5e36f2b",
+    "url": "/time_machine/static/js/2.224c4a04.chunk.js"
+  },
+  {
+    "revision": "b9f28c176efdd716457c02939f4eac67",
+    "url": "/time_machine/static/js/2.224c4a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f5d2d66e5d1b3789210",
+    "url": "/time_machine/static/js/main.d94de7c5.chunk.js"
+  },
+  {
+    "revision": "b9f5bcffbd235a89cb13",
+    "url": "/time_machine/static/js/runtime-main.fc327eb2.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/time_machine/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/time_machine/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/time_machine/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "f721466883998665b87923b92dea655b",
+    "url": "/time_machine/static/media/glyphicons-halflings-regular.f7214668.svg"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/time_machine/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "89b41a5ce07c662c6c80194cbaf877a1",
+    "url": "/time_machine/static/media/pfw-white.89b41a5c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/time_machine/precache-manifest.a149fdb2c2538ca559e805fb87551f42.js"
+  "/time_machine/precache-manifest.9cf7afccc48e8241a1d3b4bbf0c1926d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/time_machine/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/time_machine/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
