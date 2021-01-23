82c07097939c73561bcd91d0c5f1093a28bc9901
Joel Stauffer <altosax13@gmail.com>
2021-01-23 09:29:31 -0500

    wip: working on converting File Utilities

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -9,26 +9,29 @@ const FileInput = ({ title, accept, isPlot, onDone }: FileInputProps) => {
   const DEFAULT_FILE_NAME = "Upload file";
   const renderedTitle = title || DEFAULT_FILE_NAME;
 
-  const noFiles = (files: FileArray) => files.length === 0;
-  
+  const thereIsAtLeastOneFile = (files: FileArray) => files.length > 0;
+
   const processUpload = (files: FileArray) => {
-    if(noFiles(files))
-      return;
+    if (thereIsAtLeastOneFile(files))
+      processFileInput(files);
+  }
+
+  const processFileInput = (files: FileArray) => {
     let fileUtil = new FileUtilities();
     isPlot
       ? fileUtil.processPlotsData(files[0], onDone)
       : fileUtil.processXLSXIntoCSV(files[0], onDone);
   }
-  
+
   return (
     <FormGroup>
       <ControlLabel htmlFor={renderedTitle} style={{ cursor: "pointer" }}>
-          {renderedTitle}
-          <FormControl id={renderedTitle}
-                        type="file" 
-                        accept={accept}
-                        onChange={e => processUpload((e.target as any).files)} 
-                        style={{ display: "none" }} />
+        {renderedTitle}
+        <FormControl id={renderedTitle}
+          type="file"
+          accept={accept}
+          onChange={e => processUpload((e.target as any).files)}
+          style={{ display: "none" }} />
       </ControlLabel>
     </FormGroup>
   );
--- src/lib/utilities/FileUtilities.ts
+++ src/lib/utilities/FileUtilities.ts
@@ -1,74 +1,82 @@
-export default class FileUtilities {
-
-  processXLSXIntoCSV(xlsxFile, onComplete) {
-    var reader = new FileReader();
-    reader.readAsBinaryString(xlsxFile);
-    reader.onload = () => {
-      var csvSheetStrings = this.transformXLXSIntoCsvStrings(reader.result);
-      onComplete(this.parseSingleCSV(csvSheetStrings[0]));
-    }
-  }
-
-  processPlotsData(file, onComplete){
-    var reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = () => {
-      let text = reader.result;
-      onComplete(JSON.parse(text));
-    }
-  }
-
-  transformXLXSIntoCsvStrings(binaryContents) {
-    const XLSX = require('xlsx')
-    const binaryType = {
-      type: 'binary'
-    };
-    var csvStrings = [];
-    var workbook = XLSX.read(binaryContents, binaryType);
-    workbook.SheetNames.forEach(name => {
-      csvStrings.push(XLSX.utils.sheet_to_csv(workbook.Sheets[name]));
-    });
-
-    return csvStrings;
-  }
-
-  parseSingleCSV(csvString) {
-    var numOfRows = 0;
-    var numOfColumns = 1;
-    var ch, i;
-    //Count rows
-    for (i = 0; i < csvString.length; i++) {
-      ch = csvString.charAt(i);
-      if (ch === '\n') {
-        numOfRows++
-      }
-    }
-    //Count columns
-    ch = '';
-    i = 0;
-    while (ch !== '\n') {
-      ch = csvString.charAt(i);
-      if (ch === ',') {
-        numOfColumns++;
-      }
-      i++;
-    }
-    //Create 2D array to hold xlsx data
-    this.data = new Array(numOfRows);
-    for (i = 0; i < numOfRows; i++) {
-      this.data[i] = new Array(numOfColumns);
-    }
-    //Fill array
-    for (i = 0; i < this.data.length; i++) {
-      var rows = csvString.split("\n");
-      this.data[i] = rows[i].split(",");
-    }
-    //Remove multiple label rows, starting at 1 (after first label row)
-    for (i = 1; i < this.data.length; i++){
-      if ((this.data[i][0] === "t") || (this.data[i][1] === "-")) {
-        this.data.splice(i, 1);
-      }
-    }
-    return this.data;
+const processXLSXIntoCSV = (xlsxFile: File, onComplete: onCompleteCallback) => {
+  var reader = new FileReader();
+  reader.readAsBinaryString(xlsxFile);
+  reader.onload = () => {
+    let result = reader.result;
+    var csvSheetStrings = transformXLXSIntoCsvStrings(result as ArrayBuffer);
+    onComplete(parseSingleCSV(csvSheetStrings[0]));
   }
 }
+
+// TODO: Rename this to process JSON text file
+const processPlotsData = (jsonTextFile: File, onComplete: onCompleteCallback) => {
+  var reader = new FileReader();
+  reader.readAsText(jsonTextFile);
+  reader.onload = () => {
+    let text = reader.result;
+    if (typeof text === "string")
+      onComplete(JSON.parse(text));
+  }
+}
+
+const transformXLXSIntoCsvStrings = (binaryContents: ArrayBuffer) => {
+  const XLSX = require('xlsx');
+  const binaryType = {
+    type: 'binary'
+  };
+  var csvStrings = new Array<String>();
+  var workbook = XLSX.read(binaryContents, binaryType);
+  workbook.SheetNames.forEach((name: string) => {
+    csvStrings.push(XLSX.utils.sheet_to_csv(workbook.Sheets[name]));
+  });
+
+  return csvStrings;
+}
+
+// TODO: Separate the concerns of loading in the data, and which data to ignore/process
+const parseSingleCSV = (csvString: string) => {
+  var numOfRows = 0;
+  var numOfColumns = 1;
+  var ch, i;
+  //Count rows
+  for (i = 0; i < csvString.length; i++) {
+    ch = csvString.charAt(i);
+    if (ch === '\n') {
+      numOfRows++
+    }
+  }
+  //Count columns
+  ch = '';
+  i = 0;
+  while (ch !== '\n') {
+    ch = csvString.charAt(i);
+    if (ch === ',') {
+      numOfColumns++;
+    }
+    i++;
+  }
+  //Create 2D array to hold xlsx data
+  let data = new Array<Array<string>>(numOfRows);
+  for (i = 0; i < numOfRows; i++) {
+    data[i] = new Array<string>(numOfColumns);
+  }
+  //Fill array
+  for (i = 0; i < data.length; i++) {
+    var rows = csvString.split("\n");
+    data[i] = rows[i].split(",");
+  }
+  //Remove multiple label rows, starting at 1 (after first label row)
+  for (i = 1; i < data.length; i++) {
+    if ((data[i][0] === "t") || (data[i][1] === "-")) {
+      data.splice(i, 1);
+    }
+  }
+  return data;
+}
+
+type onCompleteCallback = (dataString: string) => string;
+
+export {
+  processXLSXIntoCSV,
+  processPlotsData 
+};
